5751175d15bca3356d1d4c4a7ca4084d58435680
rexale <alexander.rex@mail.com>
2023-10-05 14:32:18 +0200

    update

--- teamwork_exercise_1.m
+++ teamwork_exercise_1.m
@@ -22,10 +22,10 @@ lambda_c = c/f_c;
 
 G_bs = (4*pi)/lambda_c^2*Ae_bs;
 G_uav = 1;
-R_max = 6000;
+R_max = 28.6*1000;
 pathLoss_max = G_uav*G_bs*(lambda_c/(4*pi*R_max))^2;
 
-rainLoss_dB = -1; % dB/km
+rainLoss_dB = -0.1; % dB/km
 pathLoss_max = pathLoss_max*(10^(rainLoss_dB/10))^(R_max/1000);
 
 F_dB = 13;
@@ -44,7 +44,15 @@ P_t = SNR_in*N_in/pathLoss_max;
 totalArea = 400*60*10^6 + 60*60*10^6;
 
 delta_h = 100; 
-d = sqrt(R_max^2-delta_h^2);
-a_hex = sqrt(3/4)*d;
-A_hex = 3*a_hex*d;
-numberOfBS = totalArea/A_hex;
+d_cov = sqrt(R_max^2-delta_h^2);
+
+N_y = @(d_cov,L_b) ceil(L_b/(2*d_cov*cosd(30))+1/2);
+N_x = @(d_cov,L_a) ceil((L_a-d_cov*sind(30))/(3*d_cov*sind(30)));
+
+N_x1 = N_x(d_cov,400*1000);
+N_y1 = N_y(d_cov,60*1000);
+
+N_x2 = N_x(d_cov,60*1000);
+N_y2 = N_y(d_cov,60*1000);
+
+numberOfBS = N_x1*N_y1+N_x2*N_y2;
